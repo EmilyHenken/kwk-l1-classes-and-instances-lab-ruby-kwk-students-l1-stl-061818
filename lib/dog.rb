97346600374e1f533bb9e0ce1f991439bcc56ba1
EmilyHenken <20henkee@corjesu.org>
2018-06-21 21:28:10 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,6 @@
 # dog.rb
 
-class Ruby
+class Dog
   attr_accessor :name, :owner_name, :fur_color
   
   def initialize(name, owner_name, fur_color)
@@ -10,8 +10,8 @@ class Ruby
   end
 end
 
-dog1 = Ruby.new("Fido", "Pete", "red")
-dog2 = Ruby.new("Puppy", "Camille", "brown")
+dog1 = Dog.new("Fido", "Pete", "red")
+dog2 = Dog.new("Puppy", "Camille", "brown")
 
 puts dog2.fur_color
 puts dog1.owner
