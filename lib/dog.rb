98b2e43f38c76ed88ab684fbd7fb04d91b27ba61
EmilyHenken <20henkee@corjesu.org>
2018-06-21 21:20:15 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,3 +8,10 @@ class Ruby
     @owner_name = owner_name
     @fur_color = fur_color
   end
+end
+
+dog1 = Ruby.new("Fido", "Pete", "red")
+dog2 = Ruby.new("Puppy", "Camille", "brown")
+
+puts dog2.fur_color
+puts dog1.owner_name
