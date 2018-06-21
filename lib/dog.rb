dfe5eafe5eaf3968c9cb0c01a6668bb771f6fe23
EmilyHenken <20henkee@corjesu.org>
2018-06-21 21:18:22 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,10 @@
 # dog.rb
 
 class Ruby
-  attr_accessible :
+  attr_accessible :name, :owner_name, :fur_color
   
-  def initialize()
-  
+  def initialize(name, owner_name, fur_color)
+    @name = name
+    @owner_name = owner_name
+    @fur_color = fur_color
   end
